151dd84898a887e6205c58721eb37d47bdc157f3
KyrilosKamal <kyrilloskamal2025@gmail.com>
2025-07-13 20:50:10 +0300

    ListView.generate Explaination

--- _36_list_generate/lib/main.dart
+++ _36_list_generate/lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
+  // قائمة المستخدمين اللي هنعرضها في الليست
   List users = [
     {"name": "Kyrillos", "age": 30, "Phone": '+201228011332'},
     {"name": "Andrew", "age": 29, "Phone": '+201228745965'},
@@ -22,18 +23,38 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text("List Generate")),
+        
+        // هنا بنستخدم ListView لعرض قائمة عمودية من العناصر
         body: ListView(
+          // children بتستقبل List من Widgets
           children: [
-            ...List.generate(users.length, (index) {
-              return Card(
-                elevation: 7,
-                child: ListTile(
-                  title: Text('Name: ${users[index]['name']}'),
-                  subtitle: Text('Age: ${users[index]['age']}'),
-                  leading: Text('${users[index]['Phone']}'),
-                ),
-              );
-            }),
+
+            // ... => Spread Operator
+            // معناها خد كل عنصر داخل الليست وافرده هنا كأنه عنصر لوحده جوه الـ ListView
+            ...List.generate(
+              users.length, // length: عدد المرات اللي هيتنفذ فيها الفنكشن (يعني بعدد اليوزرز)
+
+              (index) {
+                // generator: دالة بتاخد index وترجع العنصر اللي هيتبني مكانه
+                
+                return Card(
+                  elevation: 7, // ظل وارتفاع للكارت عشان يبقى بارز عن الخلفية
+                  child: ListTile(
+                    // leading: العنصر اللي بيظهر في بداية الليست تايل (هنا رقم التليفون)
+                    leading: Text('${users[index]['Phone']}'),
+
+                    // title: العنوان الرئيسي للعنصر (هنا اسم المستخدم)
+                    title: Text('Name: ${users[index]['name']}'),
+
+                    // subtitle: النص الثانوي للعنصر (هنا العمر)
+                    subtitle: Text('Age: ${users[index]['age']}'),
+                  ),
+                );
+              },
+
+              // growable: خاصية اختيارية، لو true الليست ممكن نكبرها بعد الإنشاء، القيمة الافتراضية true
+              // مش مستخدمة هنا لإننا مش محددينها فهتفضل true
+            ),
           ],
         ),
       ),
